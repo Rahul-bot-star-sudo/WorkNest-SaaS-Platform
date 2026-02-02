b78c666eb5ea7822e87858b56a239d43ace3056b
Rahul-bot-star-sudo <rahulshinde2577@gmail.com>
2026-02-02 17:55:29 +0530

    create Node Backend structure

---
+++ worknest/Node Backend/src/module1/super admin/super admin setup/setup.service.js
@@ -0,0 +1,25 @@
+// setup.service.js
+
+const SystemRepository = require('../system/system.repository')
+
+class SetupService {
+
+  constructor() {
+    this.systemRepository = new SystemRepository()
+  }
+
+  async createSuperAdmin(dto) {
+
+    // STEP-1: Guard clause – check if setup already completed
+    const isSetupDone = await this.systemRepository.isSetupCompleted()
+
+    if (isSetupDone) {
+      throw new Error('Super Admin setup already completed')
+    }
+
+    // STEP-2 se aage ka logic baad me aayega
+  }
+
+}
+
+module.exports = SetupService
