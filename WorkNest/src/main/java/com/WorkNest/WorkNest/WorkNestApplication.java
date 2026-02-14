81983f15bca161e9dadc0402c1e2236529df9410
Rahul-bot-star-sudo <rahulshinde2577@gmail.com>
2026-02-14 22:48:30 +0530

    create WorkNest project folder

---
+++ WorkNest/src/main/java/com/WorkNest/WorkNest/WorkNestApplication.java
@@ -0,0 +1,13 @@
+package com.WorkNest.WorkNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkNestApplication.class, args);
+	}
+
+}
