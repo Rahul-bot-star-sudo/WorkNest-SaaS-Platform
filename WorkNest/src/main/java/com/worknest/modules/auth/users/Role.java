b0944c9b44daf8daeb1a9449d75deb3ea0bb1d06
Rahul-bot-star-sudo <rahulshinde2577@gmail.com>
2026-03-29 18:38:43 +0530

    create role class

---
+++ WorkNest/src/main/java/com/worknest/modules/auth/users/Role.java
@@ -0,0 +1,13 @@
+package com.worknest.modules.auth.users;
+
+// ==============================
+// STEP 2: Create Role Enum
+// MICRO-STEPS:
+// 1. Create enum Role   <-- CURRENT STEP
+// 2. Add SUPER_ADMIN
+// 3. Add other roles (optional)
+// ==============================
+
+public enum Role {
+
+}
