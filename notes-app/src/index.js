bbe3c8f6e3f03255ba36ac3c1e0ac1c83496c705
Rahul-bot-star-sudo <rahulshinde2577@gmail.com>
2026-04-14 23:09:40 +0530

    saperat all themes

--- notes-app/src/index.js
+++ notes-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ThemeContextProvider from './context/ThemeContext';
 import './styles/global.css';
+import "./styles/theme.css";
 
 import "./styles/themes/las-vegas.css";
 import "./styles/themes/casino-royale.css";
@@ -12,10 +13,17 @@ import "./styles/themes/dark.css";
 import "./styles/themes/light.css";
 import "./styles/themes/green.css";
 import "./styles/themes/ocean.css";
-import "./styles/themes/theme.css";
 import "./styles/themes/forest.css";
 import "./styles/themes/peach.css";
 import "./styles/themes/github.css";
+import "./styles/themes/sunset.css";
+import "./styles/themes/galaxy.css";
+import "./styles/themes/zenith-dark.css";
+import "./styles/themes/golden.css";
+import "./styles/themes/lavender.css";
+import "./styles/themes/vscode.css";
+
+
 // Add this line
 import './index.css';
 
